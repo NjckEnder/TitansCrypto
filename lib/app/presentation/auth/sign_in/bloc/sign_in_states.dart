c93e60bb4e6dae1822840539fd4e3addc8a2aa5b
Ender <njckname.0co@gmail.com>
2023-06-16 17:16:08 +0700

    Sign in bloc

--- lib/app/presentation/auth/sign_in/bloc/sign_in_states.dart
+++ lib/app/presentation/auth/sign_in/bloc/sign_in_states.dart
@@ -3,16 +3,30 @@ part of sign_in_blocs;
 class SignInState {
   final String email;
   final String password;
+  final bool isValid;
+  final String? errorMessage;
 
   const SignInState({
     this.email = "",
     this.password = "",
+    this.isValid = false,
+    this.errorMessage,
   });
 
-  SignInState copyWith({String? email, String? password}) {
+  @override
+  List<Object?> get props => [email, password, isValid, errorMessage];
+
+  SignInState copyWith({
+    String? email,
+    String? password,
+    bool? isValid,
+    String? errorMessage,
+  }) {
     return SignInState(
       email: email ?? this.email,
       password: password ?? this.password,
+      isValid: isValid ?? this.isValid,
+      errorMessage: errorMessage ?? this.errorMessage,
     );
   }
 }
